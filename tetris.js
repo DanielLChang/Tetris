784110dcc0f4abb8d21b4e9fc4b42e9a9e7f29cf
DanielLChang <daniellawrencechang@gmail.com>
2017-04-30 18:20:35 -0700

    Add grid

--- tetris.js
+++ tetris.js
@@ -95,17 +95,26 @@ function drawMatrix(matrix, offset) {
     matrix.forEach((row, y) => {
         row.forEach((value, x) => {
             if (value !== 0) {
-                context.fillStyle = colors[value];
-                context.fillRect(x + offset.x,
-                                 y + offset.y,
-                                 1, 1);
+              context.beginPath();
+              context.rect(x + offset.x, y + offset.y, 1, 1);
+              context.fillStyle = colors[value];
+              context.fill();
+              context.lineWidth = .05;
+              context.strokeStyle="black";
+              context.stroke();
+            } else {
+              context.beginPath();
+              context.rect(x, y, 1, 1);
+              context.lineWidth = .01;
+              context.strokeStyle = 'black';
+              context.stroke();
             }
         });
     });
 }
 
 function draw() {
-    context.fillStyle = '#000';
+    context.fillStyle = '#f7f7f7';
     context.fillRect(0, 0, canvas.width, canvas.height);
 
     drawMatrix(arena, {x: 0, y: 0});
