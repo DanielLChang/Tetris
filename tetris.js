c73ef986e88f070ea7b158fd8be8f7aebf8dba04
DanielLChang <daniellawrencechang@gmail.com>
2017-04-30 18:01:39 -0700

    Collision and create arena

--- tetris.js
+++ tetris.js
@@ -3,17 +3,43 @@ const ctx = canvas.getContext('2d');
 
 ctx.scale(20, 20);
 
-function draw() {
-  ctx.fillStyle = '#000';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  drawMatrix(player.matrix, player.pos);
-}
-
 const matrix = [
   [0, 0, 0],
   [1, 1, 1],
   [0, 1, 0],
 ];
+
+function collide(arena, player) {
+  const m = player.matrix;
+  const o = player.pos;
+  for (let y = 0; y < m.length; ++y) {
+    for (let x = 0; x < m[y].length; ++x) {
+      if (m[y][x] !== 0
+        && arena[y + o.y]
+        && arena[y + o.y][x + o.x] !== 0) {
+          return true;
+        }
+    }
+  }
+  return false;
+}
+
+function createMatrix(width, height) {
+  const matrix = [];
+  while (height--)  {
+    matrix.push(new Array(width).fill(0));
+  }
+
+  return matrix;
+}
+
+function draw() {
+  ctx.fillStyle = '#000';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  drawMatrix(arena, {x: 0, y: 0});
+  drawMatrix(player.matrix, player.pos);
+}
 
 function drawMatrix(matrix, offset) {
   matrix.forEach((row, y) => {
@@ -26,27 +52,59 @@ function drawMatrix(matrix, offset) {
   });
 }
 
+function merge(arena, player) {
+  player.matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        arena[y + player.pos.y][x + player.pos.x] = value;
+      }
+    });
+  });
+}
+
+function playerDrop() {
+  player.pos.y++;
+  if (collide(arena, player)) {
+    player.pos.y--;
+    merge(arena, player);
+    player.pos.y = 0;
+  }
+  dropCounter = 0;
+}
+
 let dropCounter = 0;
 let dropInterval = 1000;
 
 let lastTime = 0;
 function update(time = 0) {
   const deltaTime = time - lastTime;
-  lastTime = time;
 
   dropCounter += deltaTime;
   if (dropCounter > dropInterval) {
-    player.pos.y++;
-    dropCounter = 0;
+    playerDrop();
   }
+
+  lastTime = time;
 
   draw();
   requestAnimationFrame(update);
 }
 
+const arena = createMatrix(12, 20);
+
 const player = {
   pos: {x: 5, y: 5},
   matrix: matrix,
 };
+
+document.addEventListener('keydown', event => {
+  if (event.keyCode === 37) {
+    player.pos.x--;
+  } else if (event.keyCode === 39) {
+    player.pos.x++;
+  } else if (event.keyCode === 40) {
+    playerDrop();
+  }
+});
 
 update();
