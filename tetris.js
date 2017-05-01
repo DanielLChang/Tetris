56971727d9da809a60208c5927249da033f4e915
DanielLChang <daniellawrencechang@gmail.com>
2017-05-01 10:11:48 -0700

    Create player class

--- tetris.js
+++ tetris.js
@@ -8,7 +8,7 @@ function arenaSweep() {
   outer: for (let y = arena.length -1; y > 0; --y) {
     for (let x = 0; x < arena[y].length; ++x) {
       if (arena[y][x] === 0) {
-          continue outer;
+        continue outer;
       }
     }
 
@@ -247,11 +247,7 @@ const colors = [
 
 const arena = createMatrix(12, 20);
 
-const player = {
-  pos: {x: 0, y: 0},
-  matrix: null,
-  score: 0,
-};
+const player = new Player;
 
 playerReset();
 updateScore();
